f0a41682c0335dd8fd73bd971af9ac50808b7a24
ammpitts <ambermpitts29@gmail.com>
2019-02-28 21:36:51 -0700

    non-numerical value input

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,48 @@ $(document).ready(function(){
         $(".nav-wrapper").css("background-color",colorValue);
         $("#foot").css("background-color",colorValue);
     })
-})
+
+
+    // $(".first_name")=function validate() {
+    //     if (document.myForm.name.value == "") {
+    //         alert("Enter a name");
+    //         document.myForm.name.focus();
+    //         return false;
+    //     }
+    //     if (!/^[0-9]*$/g.test(document.myForm.name.value)) {
+    //         alert("Invalid characters");
+    //         document.myForm.name.focus();
+    //         return false;
+    //     }
+    // }
+
+    $('#register-button').click(function(){
+
+        let firstName = hasNumber($('#first_name').val());
+        let lastName = hasNumber($('#last_name').val());
+
+        console.log ("DOES FIRST NAME HAVE A NUMBER?", firstName, "DOES LAST NAME HAVE A NUMBER?:", lastName)
+
+        if (firstName || lastName){
+            alert('NO NUMBERS PLEASE!')
+        }else{
+
+            //PUT CODE TO REGISTER USER
+
+
+            alert("YOU SIGNED UP COOL!")
+        }
+
+    });
+
+
+    function hasNumber(str) {
+        return /\d/.test(str);
+      }
+
+    
+});
+
 
 
 
