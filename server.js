7c15dcf954916d8eba1d62bd5a0f14ffa8d37a79
satinder <satinder042890@gmail.com>
2019-02-28 20:59:14 -0700

    html routes working

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // Routes
 // =============================================================
