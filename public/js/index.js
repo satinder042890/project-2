3e1be68f5cb2faa589320db81c94fd9022fe7c7a
satinder <satinder042890@gmail.com>
2019-02-28 20:25:27 -0700

    personalize bug fixed

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#dropdown li").on("click",function(){
+    $("#dropdown1 li").on("click",function(){
         var colorValue=$(this).text();
         console.log(colorValue);
         $(".nav-wrapper").css("background-color",colorValue);
@@ -9,3 +9,11 @@ $(document).ready(function(){
 
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, options);
+//   });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
